36ecf7c6d17d759c51ff6d580fd77dbce1b59904
emfierro <60270967+emfierro@users.noreply.github.com>
2021-12-24 14:29:11 +0200

    11

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
 
 
+ 
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <cstring>
 
-int main(){return 0;}
+#include <iostream> 
+  
+
+
+
+int main(){
+  
+   cout<<"hello"<<endl;
+  
+  return 0;}
